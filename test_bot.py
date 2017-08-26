16f40642770ce980834c1f6c277c57d755112331
sviat <sstetsyak@gmail.com>
2017-08-26 23:27:22 +0300

    qwe

--- test_bot.py
+++ test_bot.py
@@ -5,16 +5,6 @@ import os
 TOKEN = os.environ['TGTOKEN']
 CHAT_ID = os.environ["TGCHATID"]
 
-print 'print some output'
-print os.environ['testvar']
-
-testvar = os.environ['testvar']
-
-#testvar = "$testvar"
-#testvar = $testvar
-
-
-print testvar
 
 URL = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=testmassage"
 get = urllib2.urlopen(URL.encode('UTF-8'))
