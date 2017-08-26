14dfdae037fcefc233887a8ec18ab162cc3144cd
sviat <sstetsyak@gmail.com>
2017-08-26 22:40:29 +0300

    qwe

---
+++ test_bot.py
@@ -0,0 +1,8 @@
+import urllib2
+
+TOKEN = "$TGTOKEN"
+CHAT_ID = "THCHATID"
+
+
+URL = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=testmassage"
+get = urllib2.urlopen(URL.encode('UTF-8'))
