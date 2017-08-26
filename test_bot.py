dbc1ede782601ad46a10f76ed4d84729879e9b5d
sviat <sstetsyak@gmail.com>
2017-08-26 22:41:06 +0300

    qwe

--- test_bot.py
+++ test_bot.py
@@ -1,7 +1,7 @@
 import urllib2
 
 TOKEN = "$TGTOKEN"
-CHAT_ID = "THCHATID"
+CHAT_ID = "TGCHATID"
 
 
 URL = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=testmassage"
