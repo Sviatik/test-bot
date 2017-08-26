188dcd4eed272c376598b6a85b849ce3216339f8
sviat <sstetsyak@gmail.com>
2017-08-26 23:03:04 +0300

    qwe

--- test_bot.py
+++ test_bot.py
@@ -3,6 +3,9 @@ import urllib2
 TOKEN = "$TGTOKEN"
 CHAT_ID = "TGCHATID"
 
+testvar = "$testvar"
+
+print testvar
 
 URL = "https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=testmassage"
 get = urllib2.urlopen(URL.encode('UTF-8'))
