19aac58c0a4aae62d4310102652cf7f96dede86e
sviat <sstetsyak@gamil.com>
2017-12-10 19:34:32 +0200

    update

--- test_bot.py
+++ test_bot.py
@@ -5,8 +5,6 @@ import requests
 TOKEN = os.environ['TGTOKEN']
 CHAT_ID = os.environ["TGCHATID"]
 
-#print 'qwe'
-
 URL = "https://api.telegram.org/bot" + TOKEN + "/"
 #?chat_id=" + CHAT_ID + "&text=testmassage"
 #get = urllib2.urlopen(URL.encode('UTF-8'))
@@ -18,4 +16,12 @@ def send_message(chat_id, text="bla bla bla"):
 	url = URL + "sendMessage" + '?chat_id=' + CHAT_ID + "&text=" + text
 	#answer = {"char_id": chat_id, "text": text}
 #	r = requests.get(url)
-	get = urllib2.urlopen(url.encode('UTF-8'))	
+	get = urllib2.urlopen(url.encode('UTF-8'))
+
+
+
+
+
+if __name__ == '__main__':
+	send_message(CHAT_ID)
+	#main()
